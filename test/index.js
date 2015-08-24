a6243f069e8e596644e9c72b76dd6c6522a0dc1a
Ant Cosentino <antony@mycosentino.com>
2015-08-24 18:30:33 +0200

    fleshed out test cases and updated pkgjson

---
+++ test/index.js
@@ -0,0 +1,38 @@
+
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('index', function() {
+
+  describe('args', function() {
+
+    it('should accept fqdn', function(done) {});
+    it('should accept ipv4', function(done) {});
+    it('should accept ipv6', function(done) {});
+
+    describe('flags', function() {
+      //
+      // json
+      //
+      // and
+      //
+      // debug
+      //
+    });
+
+    describe('argv', function() {
+
+      it('should read from argv', function(done) {});
+
+    });
+
+    describe('stdin', function() {
+
+      it('should read from stdin', function(done) {});
+
+    });
+
+  });
+
+});
